0df4fac1b2977ca02cbb5a27dc86fc3a5f46191d
Calvin Langer <calvin.langerr@gmail.com>
2022-02-12 13:30:05 +0100

    Add scroll jumo animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,39 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+"use strict";
 
+/*=============== CHANGE BACKGROUND HEADER ===============*/
 
 /*=============== SERVICES MODAL ===============*/
 
-
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
-
-/* Link active work */ 
-
+/* Link active work */
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const section = document.querySelectorAll("section[id]");
 
+function scrollActive() {
+  const scrollY = window.pageYOffset;
 
-/*=============== LIGHT DARK THEME ===============*/ 
+  section.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
 
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active__link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active__link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*=============== LIGHT DARK THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
